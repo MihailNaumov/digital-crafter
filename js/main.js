7d29d401efdf8c06692c9441d825558dbb102ac0
MihailNaumov <50247224+MihailNaumov@users.noreply.github.com>
2019-06-04 23:20:53 +0300

    Slider
    Top-slider added. Need work about more details.

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+/*Плавный скролл (Переделать! 
+Нужен вариант с добавлением кнопок через один общий класс)*/
 function slowScroll (id) {
     var offset = 0;
     $('html, body').animate ({
@@ -5,3 +7,34 @@ function slowScroll (id) {
     }, 1000);
     return false;
   }
+$(document).ready(function(){
+  $(".owl-carousel").owlCarousel({items:1, 
+   responsiveClass:true,
+    responsive:{
+        0:{
+        	stagePadding:0,
+        },
+        600:{
+            stagePadding:0,
+
+        },
+        800:
+        {
+        	stagePadding:0,
+        },
+        1000:
+        {
+        	stagePadding:200,
+        },
+        1200:
+        {
+        	stagePadding:300,
+        },
+        1600:
+        {
+        	stagePadding:500,
+        
+        },
+    }});
+
+});
